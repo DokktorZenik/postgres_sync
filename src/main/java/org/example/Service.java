07c76674f994b026b545ace38ac3b0dab4bf2bf7
Artem Holubiev <artem.holubiev@nure.ua>
2024-10-19 16:25:28 +0300

    Fix connection

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -8,26 +8,32 @@ import org.postgresql.jdbc.PgConnection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class Service {
 
     private static final String QUEUE_NAME = "db_changes";
-    private static final String RABBITMQ_HOST = "rabbitmq_service_address";
+    private static final String RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
     private static final String PG_LISTEN_CHANNEL = "data_changes";
+    private static final String POSTGRES_URL = System.getenv("POSTGRES_URL");;
+    public static final String RABBITMQ_USERNAME = System.getenv("RABBITMQ_USERNAME");
+    public static final String RABBITMQ_PASSWORD = System.getenv("RABBITMQ_PASSWORD");
 
     public static void main(String[] args) {
         try {
 
             Properties props = new Properties();
             props.setProperty("user", "postgres");
-            props.setProperty("password", "secret");
-            PgConnection pgConn = (PgConnection) DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", props);
+            props.setProperty("password", "postgres");
+            PgConnection pgConn = (PgConnection) DriverManager.getConnection(POSTGRES_URL, props);
             Statement stmt = pgConn.createStatement();
             stmt.execute("LISTEN " + PG_LISTEN_CHANNEL);
 
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost(RABBITMQ_HOST);
+            factory.setUsername(RABBITMQ_USERNAME);
+            factory.setPassword(RABBITMQ_PASSWORD);
             try (Connection rabbitConnection = factory.newConnection();
                  Channel channel = rabbitConnection.createChannel()) {
 
